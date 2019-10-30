877579db9adeaae2aaa6c8cfae2875626e930540
younghk <hwa1130k@gmail.com>
2019-10-31 03:43:58 +0900

    add typography.jsx

---
+++ src/typography.jsx
@@ -0,0 +1,38 @@
+import Typography from 'typography';
+
+const googleFont = [
+  {
+    name: 'Nanum Gothic Coding'
+  },
+  {
+    name: 'Nanum Gothic',
+    bold: [400, 700],
+  },
+  {
+    name: 'Noto Sans KR',
+    bold: [300, 500]
+  }
+];
+
+const typography = new Typography({
+  baseFontSize: '16px',
+  baseLineHeight: '1.5',
+  bodyWeight: '300',
+  headerWeight: '700',
+  boldWeight: '500 !important',
+  headerFontFamily: ['Nanum Gothic'],
+  bodyFontFamily: ['Noto Sans KR'],
+});
+
+const googleFontString = googleFont
+  .map(v => {
+    if (v.bold) {
+      return `${v.name.replace(/ /gi, '+')}:${v.bold.toString()}`;
+    } else {
+      return `${v.name.replace(/ /gi, '+')}`;
+    }
+  })
+  .join('|')
+  .toString();
+const { rhythm, scale } = typography;
+export { googleFontString, rhythm, scale, typography as default };
